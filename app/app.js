9766a8ea712e99db396d011b8d27200286745478
Micah J <justmic007>
2019-08-31 17:48:38 +0100

    folder and file restructure

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use((req, res, next) => {
     res.status(200).json({
-        message: 'it works'
+        message: 'It works'
     })
 })
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const http = require('http');
 
-const app = require('./app');
+const app = require('./app/app');
 
 const port = process.env.PORT || 3000;
 
