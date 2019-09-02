5b154837cce9eab82d733f08df36ecc6d3dc8f9e
Micah J <justmic007>
2019-09-02 07:53:15 +0100

    improve project setup

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
 const logger = require('morgan');
 
 const app = express();
@@ -8,6 +8,9 @@ const app = express();
 const productRoutes = require('./routes/products');
 const oderRoutes = require('./routes/orders');
 
+app.use(logger('dev'));
+
+//Routes that handles requests
 app.use('/products', productRoutes);
 app.use('/orders', oderRoutes);
 
