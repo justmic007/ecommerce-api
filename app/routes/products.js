e9f7ebc9323e0b46bd9e8ee13c305557b6899b38
Micah J <justmic007>
2019-09-12 08:51:42 +0100

    added  documentation to the other  routes

--- app/routes/products.js
+++ app/routes/products.js
@@ -22,8 +22,23 @@ router.post('/', (req, res) => {
         .then(payload => {
             console.log(payload);
             res.status(201).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: payload
+                message: 'Created product successfully',
+                createdProduct: {
+                    productName: payload.productName,
+                    price: payload.price,
+                    serialNumber: payload.serialNumber,
+                    productSKU: payload.productSKU,
+                    brand: payload.brand,
+                    model: payload.model,
+                    category: payload.category,
+                    manufacturer: payload.manufacturer,
+                    description: payload.description,
+                    uuid: payload.uuid,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + payload.uuid
+                    }
+                }
             });
         })
         .catch(err => {
@@ -82,9 +97,15 @@ router.get(`/:productUUID`, (req, res) => {
     Product.findOne({ uuid, 'meta.active': { $gte: true } }, {__v: 0, _id: 0})
     .exec()
     .then(payload => {
-        console.log(payload);
+        console.log("From database", payload);
         if (payload) {
-            res.status(200).json(payload);
+            res.status(200).json({
+                product: payload,
+                request: {
+                    type: 'GET',
+                    description: 'Get a Single product',
+                    url: 'http://localhost:3000/products/' + payload.uuid
+            }});
         } else {
             res.status(404).json({ message: 'Not found'});
         }
@@ -103,10 +124,14 @@ router.put('/:productUUID', (req, res) => {
     }
     Product.updateOne({ uuid: uuid }, { $set: updateOps, 'meta.updated': Date.now()})
         .exec()
-    .then(payload => {
-        res.status(200).json({
-            payload
-        })
+        .then(payload => {
+            res.status(200).json({
+                product: payload,
+                request: {
+                    type: 'PUT',
+                    description: 'Updates a Single product',
+                    url: 'http://localhost:3000/products/' + uuid
+        }})
     })
     .catch(err => {
         res.status({
@@ -125,9 +150,13 @@ router.delete('/:productUUID', (req, res) => {
     )
     .exec()
     .then(payload => {
-        res.status(200).json({
-            payload
-        })
+            res.status(200).json({
+                product: payload,
+                request: {
+                    type: 'DELETE',
+                    description: 'Soft-deletes a single product by its uuid',
+                    url: 'http://localhost:3000/products/'
+        }})
     })
     .catch(err => {
         console.log(err);
