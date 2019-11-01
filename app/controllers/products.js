20beb12e2ae5cd558ed4d7456ee7f68d5ee76689
Micah J <justmic007>
2019-11-01 11:40:33 +0100

    moved products GetOne, UpdateOne, DeleteOne, route logic to controllers

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -51,3 +51,123 @@ exports.productsPOST = (req, res) => {
             })
         });
 }
+
+exports.productsGetAll = (req, res) => {
+    Product.find({'meta.active': { $gte: true }}, {__v: 0, _id: 0})
+    .exec()
+    .then(payload => {
+        const response = {
+            count: payload.length,
+            products: payload.map(payload => {
+                return {
+                    productName: payload.productName,
+                    productImage: payload.productImage,
+                    price: payload.price,
+                    serialNumber: payload.serialNumber,
+                    productSKU: payload.productSKU,
+                    brand: payload.brand,
+                    model: payload.model,
+                    category: payload.category,
+                    manufacturer: payload.manufacturer,
+                    description: payload.description,
+                    uuid: payload.uuid,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + payload.uuid
+                    }
+                }}),
+        };
+        console.log(payload)
+        // if (payload) {
+            res.status(200).json({
+                response
+        })
+        // } else {
+        //     res.status(404).json({
+        //         message: 'Not found'
+        //     })
+        // }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+}
+
+exports.productsGetOne = (req, res) => {
+    const uuid = req.params.productUUID;
+    Product.findOne({ uuid, 'meta.active': { $gte: true } }, {__v: 0, _id: 0})
+    .exec()
+    .then(payload => {
+        console.log("From database", payload);
+        if (payload) {
+            res.status(200).json({
+                product: payload,
+                request: {
+                    type: 'GET',
+                    description: 'Get a Single product',
+                    url: 'http://localhost:3000/products/' + payload.uuid
+            }});
+        } else {
+            res.status(404).json({ message: 'Not found'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+}
+
+exports.productsUpdateOne = (req, res) => {
+    const uuid = req.params.productUUID;
+    const updateOps = {};
+    for (const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({ uuid: uuid }, { $set: updateOps, 'meta.updated': Date.now()})
+        .exec()
+        .then(payload => {
+            res.status(200).json({
+                product: payload,
+                message: 'Product updated',
+                request: {
+                    type: 'PUT',
+                    description: 'Updates a Single product',
+                    url: 'http://localhost:3000/products/' + uuid
+        }})
+    })
+    .catch(err => {
+        res.status({
+            error: err
+        })
+    })
+}
+
+exports.productsDeleteOne = (req, res) => {
+    const uuid = req.params.productUUID;
+    Product.updateOne(
+        { uuid },
+        {
+            $set: { 'meta.active': false, 'meta.updated': new Date() }
+        }
+    )
+    .exec()
+    .then(payload => {
+            res.status(200).json({
+                product: payload,
+                message: 'Product deleted',
+                request: {
+                    type: 'DELETE',
+                    description: 'Soft-deletes a single product by its uuid',
+                    url: 'http://localhost:3000/products/'
+        }})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    });
+}
